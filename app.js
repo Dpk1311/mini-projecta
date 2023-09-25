be760bb5810fe1a3dc84ded2f06d6ba16931e764
Deepak S <s.deepak73333@gmail.com>
2023-09-25 14:50:43 +0530

    nav bar added

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 
 app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
 
 
